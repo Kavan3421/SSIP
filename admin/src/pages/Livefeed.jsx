c05afcf5c72a662d3096a8bcc4bb20b788a72c9e
Kavan Patel <kavanpatel3421@gmail.com>
2025-01-19 22:39:55 +0530

    Changes in admin

--- admin/src/pages/Livefeed.jsx
+++ admin/src/pages/Livefeed.jsx
@@ -1,11 +1,90 @@
-import React from 'react'
+// LiveFeed.js
+import React, { useEffect, useRef } from "react";
+import styled from "styled-components";
 
-const Livefeed = () => {
+// Styled Components
+const Container = styled.div`
+  flex:1;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  height: 100%;
+  padding: 22px 0px;
+  overflow-y: scroll;
+`;
+
+const VideoWrapper = styled.video`
+  width: 50%;
+  height: auto;
+  border: 5px solid ${(props) => props.theme.primary};
+  box-shadow: 0 4px 8px ${(props) => props.theme.shadow};
+  border-radius: 8px;
+  background-color: ${(props) => props.theme.bgLight};
+`;
+
+const Section = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  padding: 0px 16px;
+  gap: 22px;
+  padding: 0px 16px;
+  @media (max-width: 600px) {
+    gap: 12px;
+  }
+`;
+
+const Title = styled.div`
+  padding: 0px 16px;
+  font-size: 22px;
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: 500;
+`;
+
+const LiveFeed = () => {
+  const videoRef = useRef(null);
+  const videoRef2 = useRef(null);
+
+  useEffect(() => {
+    // Replace 'http://your-backend-url/live-feed' with your backend's stream URL
+    const streamUrl =
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+    const streamUrl2 =
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4";
+    if (videoRef.current) {
+      videoRef.current.src = streamUrl;
+    }
+    if (videoRef2.current) {
+      videoRef2.current.src = streamUrl2;
+    }
+  }, []);
+
   return (
-    <div>
-      Live telecast is here...
-    </div>
-  )
-}
+    <Container>
+      <Section>
+        <Title>Entry</Title>
+        <VideoWrapper
+          ref={videoRef}
+          autoPlay
+          controls
+          controlsList="nodownload"
+          muted
+        >
+          Your browser does not support the video tag.
+        </VideoWrapper>
+        <Title>Exit</Title>
+        <VideoWrapper
+          ref={videoRef2}
+          autoPlay
+          controls
+          controlsList="nodownload"
+          muted
+        >
+          Your browser does not support the video tag.
+        </VideoWrapper>
+      </Section>
+    </Container>
+  );
+};
 
-export default Livefeed
+export default LiveFeed;
