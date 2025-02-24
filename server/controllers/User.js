79a300b64d9e154855ea4a66877d25f686d39870
Kavan Patel <kavanpatel3421@gmail.com>
2025-02-24 12:32:15 +0530

    update QR code image URL to use production endpoint

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -279,7 +279,7 @@ export const generateQrCode = async (req, res) => {
     // Send the Image URL & Database Entry Confirmation
     res.status(200).json({
       message: "QR Code generated and stored successfully!",
-      qrImageUrl: `http://localhost:8080/qrcodes/${fileName}`,
+      qrImageUrl: `https://ssip-xv3o.onrender.com/qrcodes/${fileName}`,
       qrData,
     });
 
